164a09cbf9ebe6e78773d2b6d5a3bbeb93040d9e
Aditya boxi <pier_looper_8@icloud.com>
2026-06-17 01:38:50 +0530

    hi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,7 +98,7 @@ function Home() {
             isWhiteTheme ? "text-gray-500 hover:text-gray-700" : "text-gray-400 hover:text-white"
           }`}
         >
-          API Historyyyyyy {isApiHistoryOpen ? ">>" : "<<"}
+          API History {isApiHistoryOpen ? ">>" : "<<"}
         </button>
 
         <span className={`font-medium ${isWhiteTheme ? "text-gray-500" : "text-gray-400"}`}>
